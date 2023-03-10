fece0a2e403339ee798372416dc8ab90cc9a7b7f
pjevic <milanpjevic@icloud.com>
2023-03-10 14:30:13 +0100

    Setting up the Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-table">
+        <div className="navigation">navigation</div>
+        <div className="container-table--title">employees</div>
+        <div className="container-table--body">data</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
