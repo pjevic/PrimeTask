4e090afec8a13be6acee9f142ad49f1e818a70ed
pjevic <milanpjevic@icloud.com>
2023-03-10 17:27:13 +0100

    Adding Navigation Component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "./index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- src/routes/00_Navigation/Navigation.js
+++ src/routes/00_Navigation/Navigation.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+
+import logo from "../../assets/images/logo-PrimeTask.png";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>Navigation</div>
+    <>
+      <div>
+        <Link to={"/employees"}>Employees</Link>
+        <Link to={"/"}>
+          <img src={logo} alt="Logo" />
+        </Link>
+        <Link to={"/tasks"}>Tasks</Link>
+      </div>
       <Outlet />
-    </div>
+    </>
   );
 };
 
